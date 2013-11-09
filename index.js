da575fac44285d1666925a65a99bd6048fa7e08a
Michael Toth <tofudrummer@gmail.com>
2013-11-08 21:44:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var request = require("request");
+var express = require("express");
+
+var gif_response = "";
+var app = express();
+
+app.use(express.logger);
+
+app.get("*", function(req, response) {
+
+    console.log("Yay!");
+});
+
+var port = process.env.PORT || 24601;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
