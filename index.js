54db3e1110d217c7d58ee00f16474164d8c7babd
Michael Toth <tofudrummer@gmail.com>
2013-11-08 22:54:41 -0500

    merp

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, response) {
-
-    console.log("geez");
+    response.send("Hello World");
+    console.log("I see...");
 });
 
-var port = process.env.PORT || 24601;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
