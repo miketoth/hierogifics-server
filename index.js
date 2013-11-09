7dd2180448c72c75c3103585c585fbc4978685d0
Michael Toth <tofudrummer@gmail.com>
2013-11-08 22:48:18 -0500

    working it

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.get('/', function(req, response) {
+
+    console.log("geez");
+});
+
 var port = process.env.PORT || 24601;
 app.listen(port, function() {
     console.log("Listening on " + port);
