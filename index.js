d65cb97de7f6d7462431e6cfff316859da3b0522
Michael Toth <tofudrummer@gmail.com>
2013-11-08 23:14:31 -0500

    Added giphy querying

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+var request = require("request");
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, response) {
+var gif_response = "";
+
+app.get('/:gif_type', function(req, response) {
     response.send("Hello World");
     console.log("I see...");
+
+    request("http://api.giphy.com/v1/gifs/search?q=" + req.params["gif_type"] + "&limit=100&api_key=dc6zaTOxFJmzC", function(error, response, body) {
+        if(error) {
+            console.log("failed to query api");
+        }
+        else {
+            var json_response = JSON.parse(body);
+            response.send(json_response);
+        }
+    });
 });
 
 var port = process.env.PORT || 5000;
