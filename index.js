94756898268593f8b9883eea186eedbd5f36229c
Michael Toth <tofudrummer@gmail.com>
2013-11-08 22:45:42 -0500

    wat

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-var request = require("request");
 var express = require("express");
-
-var gif_response = "";
 var app = express();
-
-app.use(express.logger);
-
-app.get("/hi", function(req, response) {
-    response.send("Hello World");
-});
 
 var port = process.env.PORT || 24601;
 app.listen(port, function() {
